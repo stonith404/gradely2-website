b322267abb02c2e7bc5681a8058066b63f3d4148
Elias Schneider <login@eliasschneider.com>
2021-10-05 18:53:40 +0200

    inital commit

---
+++ src/components/header.svelte
@@ -0,0 +1,31 @@
+<script>
+
+</script>
+<nav class="navbar navbar-light">
+    <a class="navbar-brand" href="https://gradelyapp.com">
+      <img src="/img/gradely2logo.png" width="50" height="50" class="d-inline-block align-center" alt="">
+     Gradely 2
+    </a>
+    <a href="#download">Download</a>
+  </nav>
+
+<style>
+  a{
+    text-decoration: none !important;
+    color: black;
+    margin-right: 40px ;
+  }
+  a:hover{
+    text-decoration: none !important;
+    color: rgb(58, 58, 58);
+
+  }
+    .navbar-brand{
+        font-size: 15px;
+        margin: 8px;
+    }
+    nav{
+margin: -8px;
+        
+    }
+</style>
