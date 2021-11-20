9bc2dbaccc21336360b6c22045458a0c06938363
Elias Schneider <login@eliasschneider.com>
2021-11-20 14:07:09 +0100

    fixed svelte adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import preprocess from 'svelte-preprocess';
+import node from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -6,7 +7,7 @@ const config = {
 	// for more information about preprocessors
 	preprocess: preprocess(),
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
+		adapter: node(),
 		target: '#svelte'
 	}
 };
